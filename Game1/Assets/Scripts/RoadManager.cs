e29c60ee6a0ab5ec2ee31fe3486fbd0a6167e964
jiseok08 <heoji0912@gmail.com>
2025-05-14 19:43:59 +0900

    (2025.5.14) 도로 관리자 시스템

---
+++ Game1/Assets/Scripts/RoadManager.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class RoadManager : MonoBehaviour
+{
+    [SerializeField] float speed;
+    [SerializeField] List<GameObject> roads;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for (int i = 0; i < roads.Count; i++)
+        {
+            roads[i].transform.Translate(speed * Vector3.back * Time.deltaTime);
+        }
+    }
+}
