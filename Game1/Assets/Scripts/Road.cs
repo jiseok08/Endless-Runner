c514310eb9d45200080116da699946a42019cda0
jiseok08 <heoji0912@gmail.com>
2025-05-16 14:32:17 +0900

    (2025.5.15) 도로 위치 초기화 시스템

---
+++ Game1/Assets/Scripts/Road.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class Road : MonoBehaviour, Collidable
+{
+    [SerializeField] UnityEvent callback;
+    public void Activate()
+    {
+        if(callback != null)
+        {
+            callback.Invoke();
+        }
+    }
+
+}
--- Game1/Assets/Scripts/RoadManager.cs
+++ Game1/Assets/Scripts/RoadManager.cs
@@ -1,25 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Xml;
 using UnityEditor;
 using UnityEngine;
 
 public class RoadManager : MonoBehaviour
 {
     [SerializeField] float speed;
+    [SerializeField] float offset = 40.0f;
     [SerializeField] List<GameObject> roads;
 
-    // Start is called before the first frame update
-    void Start()
-    {
 
-    }
-
-    // Update is called once per frame
     void Update()
     {
         for (int i = 0; i < roads.Count; i++)
         {
             roads[i].transform.Translate(speed * Vector3.back * Time.deltaTime);
         }
+    }
+
+    public void InitialilizePosition()
+    {
+        GameObject newRoad = roads[0];
+
+        roads.Remove(newRoad);
+
+        float newZ = roads[roads.Count - 1].transform.position.z + offset;
+    
+        newRoad.transform.position = new Vector3(0, 0, newZ);
+
+        roads.Add(newRoad);
+    }
+
+    public void Something()
+    {
+
     }
 }
