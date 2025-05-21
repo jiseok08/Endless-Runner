01b67a64763a3105ad96f5f70429fc34d360064d
jiseok08 <heoji0912@gmail.com>
2025-05-21 14:05:00 +0900

    (2025.5.20) 장애물 활성화 시스템 (미완성)

--- Game1/Assets/Scripts/Obstacle Manager.cs
+++ Game1/Assets/Scripts/Obstacle Manager.cs
@@ -1,24 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObstacleManager : MonoBehaviour
 {
-    [SerializeField] List<GameObject> List;
-    [SerializeField] GameObject TrafficCone;
-    [SerializeField] GameObject Barrier;
-    [SerializeField] GameObject OilBarrer;
+    [SerializeField] List<GameObject> obstacles;
+
+    [SerializeField] GameObject [ ] prefab;
+
+    [SerializeField] int createCount = 5;
 
     void Start()
     {
-        int a = 0;
-        for (int i = 0; i < List.Count; i++)
-        {
-            a = UnityEngine.Random.Range();
-
-            List[i].Add
-        }
+        Create();
     }
 
+    public void Create()
+    {
+        for (int i = 0; i < createCount; i++)
+        {
+            GameObject clone = Instantiate(prefab[Random.Range(0, prefab.Length)]);
 
+            clone.SetActive(false);
+
+            obstacles.Add(clone);
+        }
+    }
 }
