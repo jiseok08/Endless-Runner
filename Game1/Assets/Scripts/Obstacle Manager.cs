d1fa89fe1f4dc266700804eb088ee0e5278d0618
jiseok08 <heoji0912@gmail.com>
2025-05-23 09:49:03 +0900

    (2025.5.20) 장애물 활성화 시스템

--- Game1/Assets/Scripts/Obstacle Manager.cs
+++ Game1/Assets/Scripts/Obstacle Manager.cs
@@ -5,26 +5,67 @@ using UnityEngine;
 
 public class ObstacleManager : MonoBehaviour
 {
+    [SerializeField] int random;
+
+    [SerializeField] int createCount = 5;
+
     [SerializeField] List<GameObject> obstacles;
 
     [SerializeField] GameObject [ ] prefab;
 
-    [SerializeField] int createCount = 5;
+    [SerializeField] Transform [ ] transforms; 
 
     void Start()
     {
         Create();
+
+        StartCoroutine(ActiveObstacle());
     }
 
     public void Create()
     {
         for (int i = 0; i < createCount; i++)
         {
-            GameObject clone = Instantiate(prefab[Random.Range(0, prefab.Length)]);
+            GameObject clone = Instantiate(prefab[Random.Range(0, prefab.Length)], transform);
 
             clone.SetActive(false);
 
             obstacles.Add(clone);
+        }
+    }
+
+    bool ExmineActive()
+    {
+        for (int i = 0; i < obstacles.Count;i++)
+        {
+            if (obstacles[i].activeSelf ==  false)
+            {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public IEnumerator ActiveObstacle()
+    {
+        while(true)
+        {
+            random = Random.Range(0, obstacles.Count);
+
+            // 현재 게임 오브젝트가 활성화되어 있는지 확인합니다.
+            while (obstacles[random].activeSelf == true)
+            {
+
+                // 현재 인덱스에 있는 게임 오브젝트가 활성화되어 있으면
+                // random 변수의 값을 +1 해서 다시 검색합니다
+                random = (random + 1) % obstacles.Count;
+            }
+
+            obstacles[random].transform.position = transforms[Random.Range(0, transforms.Length)].position;
+
+            obstacles[random].SetActive(true);
+
+            yield return new WaitForSeconds(5);
         }
     }
 }
